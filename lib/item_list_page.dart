f90e9ac90cd466ac0afdce21c3e097aa047505aa
GitHob <thesn1235@gmail.com>
2024-04-29 02:50:14 +0900

    두번째 커밋

--- lib/item_list_page.dart
+++ lib/item_list_page.dart
@@ -1,7 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:project_j/item_details_page.dart';
 import 'package:project_j/models/product.dart';
-import 'package:intl/intl.dart';
+import 'constants.dart';
 
 class ItemListPage extends StatefulWidget {
   const ItemListPage({super.key});
@@ -11,8 +12,6 @@ class ItemListPage extends StatefulWidget {
 }
 
 class _ItemListPageState extends State<ItemListPage> {
-  final NumberFormat numberFormat = NumberFormat('###,###,###,###');
-
   List<Product> productList = [
     Product(
         productNo: 1,
@@ -61,6 +60,7 @@ class _ItemListPageState extends State<ItemListPage> {
           ),
           itemBuilder: (context, index) {
             return productContainer(
+              productNo: productList[index].productNo ?? 0,
               productName: productList[index].productName ?? "",
               productImageUrl: productList[index].productImageUrl ?? "",
               price: productList[index].price ?? 0,
@@ -70,38 +70,55 @@ class _ItemListPageState extends State<ItemListPage> {
   }
 
   Widget productContainer(
-      {required String productName,
+      {required int productNo,
+      required String productName,
       required String productImageUrl,
       required double price}) {
-    return Container(
-      padding: const EdgeInsets.all(5),
-      child: Column(
-        children: [
-          CachedNetworkImage(
-            imageUrl: productImageUrl,
-            height: 150,
-            fit: BoxFit.cover,
-            placeholder: (context, url) => const Center(
-              child: CircularProgressIndicator(strokeWidth: 2),
-            ),
-            errorWidget: (context, url, error) => const Center(
-              child: Text("이미지를 불러오지 못했습니다."),
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return ItemDetailsPage(
+                productNo: productNo,
+                productName: productName,
+                productImageUrl: productImageUrl,
+                price: price,
+              );
+            },
           ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: Text(
-              productName,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          children: [
+            CachedNetworkImage(
+              imageUrl: productImageUrl,
+              height: 150,
+              fit: BoxFit.cover,
+              placeholder: (context, url) => const Center(
+                child: CircularProgressIndicator(strokeWidth: 2),
+              ),
+              errorWidget: (context, url, error) => const Center(
+                child: Text("이미지를 불러오지 못했습니다."),
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: Text("${numberFormat.format(price)}원"),
-          )
-        ],
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Text(
+                productName,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Text("${numberFormat.format(price)}원"),
+            )
+          ],
+        ),
       ),
     );
   }
